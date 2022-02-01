6711250aeb71ce8291e3d1839fd8edc40f2ab159
dinara <gaynanovadr@mail.ru>
2022-02-02 00:11:59 +0100

    hw1

---
+++ src/HW_1/Moveable.java
@@ -0,0 +1,5 @@
+package HW_1;
+
+public interface Moveable extends Runnable, Jumpable{
+
+}
---
+++ src/HW_1/Obstacles.java
@@ -0,0 +1,5 @@
+package HW_1;
+
+public interface Obstacles {
+    void doObstacles(Moveable moveable);
+}
